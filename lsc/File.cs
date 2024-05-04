de9a5a33ffdd08e78403e4f47cc4334c75729698
manu <whiteelement1991@web.de>
2024-05-04 23:29:41 +0200

    Initial commit

---
+++ lsc/File.cs
@@ -0,0 +1,62 @@
+﻿namespace lsc;
+
+public class File
+{
+    private long _size;
+
+    public long Size
+    {
+        get
+        {
+            (long s, string _) = CalculateMetricPref(_size);
+            return s;
+        }
+        
+        set { _size = value; }
+    }
+
+    public long FullSize
+    {
+        get => _size;
+    }
+
+    public string MetrixPrefix {
+        get
+        {
+            (long _, string pref) = CalculateMetricPref(_size);
+            return pref;
+        }
+   }
+    public string? Name { get; set; }
+
+    public string SizeAndMetric()
+    {
+        switch (Size.ToString().Length)
+        {
+            case 1:
+                return Size +   "   "    + MetrixPrefix;
+            case 2:
+                return Size +   "  "     + MetrixPrefix;
+            case 3:
+                return Size +   " "      + MetrixPrefix;
+            case 4:
+                return Size +   ""       + MetrixPrefix;
+            default:
+                return Size +   ""       + MetrixPrefix;
+        }
+    }
+
+    private (long, string) CalculateMetricPref (long size)
+    {
+        string[] metricPref = { "b", "kb", "mb", "gb" };
+        foreach (var pref in metricPref)
+        {
+            if (size < 1000)
+                return (size, pref);
+
+            size /= 1000;
+        }
+
+        throw new Exception("Only supported for files smaller than 1 tb");
+    }
+}
---
+++ lsc/Program.cs
@@ -0,0 +1,27 @@
+﻿using ParsedFile = lsc.File;
+
+var currentDir = Environment.CurrentDirectory;
+var fileNames = Directory.GetFiles(currentDir);
+
+var parsedFiles = new List<ParsedFile>();
+
+
+// "4 Files in /temp/..."
+
+foreach (var file in fileNames)
+{
+    var fileInfo = new FileInfo(file);
+
+    var formattedFile = new ParsedFile()
+    {
+        Name = fileInfo.Name,
+        Size = fileInfo.Length
+    }; 
+    parsedFiles.Add(formattedFile);
+}
+
+foreach (var parsedFile in parsedFiles)
+{
+    Console.WriteLine(parsedFile.SizeAndMetric() + "\t" + parsedFile.Name);
+    
+}
