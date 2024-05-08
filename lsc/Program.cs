f385c540cc5f7b8eb1c060622d13257dc34fdb6f
manu <whiteelement1991@web.de>
2024-05-08 21:39:48 +0200

    - added dir support

--- lsc/Program.cs
+++ lsc/Program.cs
@@ -2,6 +2,7 @@
 using lsc;
 
 var currentDir = Environment.CurrentDirectory;
+var dirNames = Directory.GetDirectories(currentDir);
 var fileNames = Directory.GetFiles(currentDir);
 
 var parsedFiles = new List<ParsedFile>();
@@ -22,9 +23,17 @@ foreach (var file in fileNames)
 
 BraunAssert.Assert(parsedFiles.Count == fileNames.Length, "konnte nicht alle Files parsen");
 
+var parsedDirs = new List<DirectoryInfo>();
+foreach (var dir in dirNames)
+{
+    var dirInfo = new DirectoryInfo(dir);
+    parsedDirs.Add(dirInfo);
+}
+
+Console.WriteLine($"{parsedFiles.Count} files & {parsedDirs.Count} folders in {currentDir}");
+
+foreach (var parsedDir in parsedDirs)
+    Console.WriteLine($"<DIR>  {parsedDir.Name}");
 
 foreach (var parsedFile in parsedFiles)
-{
     Console.WriteLine(parsedFile.SizeAndMetric() + "\t" + parsedFile.Name);
-    
-}
